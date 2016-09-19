348b0b4767aafe724d9bd5ea2b5e7412186865cb
Alexey Kochetov <alexis.kochetov@gmail.com>
2016-09-19 22:41:02 +0300

    Initial commit of F# app

---
+++ src/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+
+type Startup() = 
+    member this.Configure(app: IApplicationBuilder) = 
+        app.Run(fun context -> context.Response.WriteAsync("Hello world!!!"))
+        
+
+[<EntryPoint>]
+let main argv = 
+    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
+    host.Run()
+    printfn "Server stopped!"
+        
+    0 // return an integer exit code
